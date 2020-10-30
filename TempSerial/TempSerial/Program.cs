55d60cad8e36a3cf8a93b587e0927b0a72d0523d
pironori <minenohiro.0620.gishinonori@gmail.com>
2020-10-30 19:39:02 +0900

    backup

---
+++ TempSerial/TempSerial/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.IO.Ports;
+using System.Text;
+
+namespace ArduinoSerial_DataToCSV
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //キー入力に使用
+            var outChar = "";
+
+            foreach (var portName in SerialPort.GetPortNames())
+            {
+                Console.WriteLine(portName);
+            }
+
+            //現在時刻
+            DateTime dt = DateTime.Now;
+            String now_time = dt.ToString();
+            now_time = now_time.Replace("/", ".").Replace(" ", "_").Replace(":", "-");
+
+            String PATH = now_time + "_SensorData.csv";
+
+            //EncodeをShift-Jisに対応
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+
+            //シリアルポート取得
+            SerialPort sp = new SerialPort("COM8");
+            sp.BaudRate = 9600;
+            sp.Parity = Parity.None;
+            sp.DataBits = 8;
+            sp.StopBits = StopBits.One;
+            sp.Handshake = Handshake.None;
+            sp.Open();
+
+            //csv出力
+            var sw = new System.IO.StreamWriter(PATH, false, System.Text.Encoding.GetEncoding("shift_jis"));
+
+            //Arduino側のサンプリング周波数
+
+
+            while (true)
+            {
+                //センサ値1行分取得
+                string data = sp.ReadLine();
+
+                //csv出力
+                sw.WriteLine(data);
+                Console.WriteLine(data);
+                sw.Flush();
+
+                //Q入力でプログラム終了
+                if (Console.KeyAvailable)
+                {
+                    outChar = Console.ReadKey().Key.ToString();
+                    if (outChar == "Q")
+                    {
+                        return;
+                    }
+                }
+
+            }
+        }
+    }
+}
---
+++ test1/test1/test1.py
@@ -0,0 +1,94 @@
+
+"""
+#num01 = 123
+#num02 = 1.23
+
+#print(type(num01))
+#print(type(num02))
+
+
+#aaa ="Hello, World"
+
+#print(aaa)
+#print(type(aaa))
+
+#a = 10
+#b = 1
+
+#bool01 = (a>b)
+
+#print(bool01)
+#print(type(bool01))
+
+#a = ["sato","minegishi","karasawa"]
+
+#a[1] = "seo"
+
+#print(a[0])
+#print(a[1])
+#print(a[2])
+
+
+#a = [["sato","minegishi"],["karasawa","seo"]]
+
+
+#print(a[0][0])
+#print(a[0][1])
+#print(a[1][0])
+#print(a[1][1])
+
+#x = 10
+#p = 2
+
+#print(x != y)
+
+#age = 0
+
+#if age >= 20:
+ #print("adalt")
+
+#elif age == 0:
+ #   print("baby")
+
+#else:
+ #print("child")
+
+#for i in range(5):
+  #  if i == 3:
+  #      break
+  #  print(i)
+
+#for i in range(5):
+ #   if i == 3:
+  #      continue
+   # print(i)
+
+#for i in range(3):
+ #   for j in range(3):
+  #      print(i,j, sep="-")
+
+#arr = [2,4,6,8,10]
+#sum = 0
+#for i in arr:
+ #   sum += i
+  #  print(sum)
+
+#def say_hello(greeting):
+ #   print(greeting)
+
+#hello = say_hello
+#hello("Good Morning")
+#def add(num01,num02):
+ #   return(num01 + num02)
+
+#add_result=add(6,2)
+#print(add_result)
+
+"""
+
+import numpy
+
+def average(num):
+    print(numpy.average(num))
+
+num = [2,4,6]
